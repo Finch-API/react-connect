3e8658c4f1d86f015fc32be4c341293e5c45cf51
Aren Patel <aren55555@users.noreply.github.com>
2021-12-13 10:39:39 -0500

    feat: Add typings (#7)
    * Add typings to the SDK. Updated the example to demonstrate usage.
    
    * Types instead of interfaces.
    
    * Bump version

---
+++ src/index.d.ts
@@ -0,0 +1,24 @@
+declare module 'react-finch-connect' {
+  export type SuccessEvent = {
+    code: string;
+  };
+
+  export type ErrorEvent = {
+    errorMessage: string;
+  };
+
+  export type ConnectOptions = {
+    clientId: string;
+    products?: string[];
+    mode?: string;
+    manual?: boolean;
+    payrollProvider?: string;
+    sandbox?: boolean;
+    onSuccess?: (e: SuccessEvent) => void;
+    onError?: (e: ErrorEvent) => void;
+    onClose?: () => void;
+    zIndex?: bigint | string;
+  };
+
+  export function useFinchConnect(opts: ConnectOptions): { open: () => void };
+}
