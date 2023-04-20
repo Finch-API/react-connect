d811c4e2e402e2de562e46490a1c80b778259605
andrewdhazlett <andrew@tryfinch.com>
2023-04-20 14:48:08 -0500

    rm extraneous rollup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,11 @@
 import commonjs from 'rollup-plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
-import resolve from 'rollup-plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import typescript from '@rollup/plugin-typescript';
 
 import pkg from './package.json';
 
-const plugins = [
-  external(),
-  resolve(),
-  commonjs(),
-  replace({ SDK_VERSION: pkg.version }),
-  typescript(),
-];
+const plugins = [external(), commonjs(), replace({ SDK_VERSION: pkg.version }), typescript()];
 
 export default [
   {
