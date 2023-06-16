45373b76e8635a56c74e94f812c3313123f1350f
Aren Patel <git@arenpatel.com>
2023-06-15 17:54:32 -0700

    Revert the defaults

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -12,9 +12,9 @@ const App = () => {
   const onClose = () => console.log('User exited Finch Connect');
 
   const { open } = useFinchConnect({
-    clientId: 'c90b78c6-2151-4ca3-8fea-ccb708ffc5d9',
+    clientId: '<your-client-id>',
     products: ['company', 'directory', 'individual', 'employment'],
-    sandbox: true,
+    // sandbox: false,
     // payrollProvider: '<payroll-provider-id>',
     onSuccess,
     onError,
